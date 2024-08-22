5548d014a392e30e34c5f440179685a4ebc98450
antonyjes <72809149@continental.edu.pe>
2024-08-22 10:29:10 -0500

    main app

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,21 @@
+import { useState } from "react"
+import Form from "./components/Form"
+
 function App() {
+  const [type, setType] = useState("length")
+
   return (
-    <>      
-    </>
+    <div className="flex flex-col items-center justify-center gap-2">
+      <h1>Unit Converter</h1>
+      <nav>
+        <ul>
+          <li><a onClick={() => setType("length")}>Length</a></li>
+          <li><a onClick={() => setType("weight")}>Weight</a></li>
+          <li><a onClick={() => setType("temperature")}>Temperature</a></li>
+        </ul>
+      </nav>
+      <Form type={type} />     
+    </div>
   )
 }
 
